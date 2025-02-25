9611d6ae9998f9b3ed8b375d537752a89fabff7f
Antonio Oliver Albert <oliver.informatico@gmail.com>
2025-02-25 21:10:00 +0100

    fix: Removed unnecessary comic attributes for this challenge

--- src/modules/comics/domain/comic.ts
+++ src/modules/comics/domain/comic.ts
@@ -2,12 +2,8 @@ import { Image } from "@/modules/shared/domain/image";
 
 export interface Comic {
   id: number;
-  digitalId: number;
   title: string;
-  issueNumber: number;
-  variantDescription: string;
   description: string;
   modified: Date;
-  isbn: string;
   thumbnail: Image;
 }
